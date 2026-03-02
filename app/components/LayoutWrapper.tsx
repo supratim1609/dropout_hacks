9f8859482fd0dc61f6c885f89b7f268a080cadd4
DiPS <dediptish10@gmail.com>
2026-03-03 02:17:02 +0530

    feat: google holi easter egg

--- app/components/LayoutWrapper.tsx
+++ app/components/LayoutWrapper.tsx
@@ -5,10 +5,12 @@ import { SpideyCursor } from "./SpideyCursor";
 import { WebShooter } from "./WebShooter";
 import { Navbar } from "./Navbar";
 import { LoadingScreen } from "./LoadingScreen";
+import { HoliEffect } from "./HoliEffect";
 
 export const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <LoadingScreen>
+            <HoliEffect />
             <SpideyCursor />
             <WebShooter />
             <Navbar />
