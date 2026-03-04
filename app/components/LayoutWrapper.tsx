f8604dce443d154410c2d82e87cf24c7ee792dd7
DiPS <dediptish10@gmail.com>
2026-03-04 20:39:56 +0530

    Revert "Merge pull request #22 from supratim1609/dips-branch"
    This reverts commit 7aff18e8b3cdea1eb35644d8cb94c5c8e6837f82, reversing
    changes made to 056ee7e0e4167e3882336a289f75985e8261f258.

--- app/components/LayoutWrapper.tsx
+++ app/components/LayoutWrapper.tsx
@@ -5,12 +5,10 @@ import { SpideyCursor } from "./SpideyCursor";
 import { WebShooter } from "./WebShooter";
 import { Navbar } from "./Navbar";
 import { LoadingScreen } from "./LoadingScreen";
-import { HoliEffect } from "./HoliEffect";
 
 export const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <LoadingScreen>
-            <HoliEffect />
             <SpideyCursor />
             <WebShooter />
             <Navbar />
